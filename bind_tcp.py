9500538c20d3d50f7eee4e901f689752c82e0916
troykkt <troydkent@gmail.com>
2016-06-20 13:05:26 -0400

    updated bind_tcp.py

--- bind_tcp.py
+++ bind_tcp.py
@@ -3,9 +3,11 @@
 import socket
 import sys
 import os
+import signal
 
 HOST = ''
 PORT = 4444
+pid = os.getpid()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -13,7 +15,7 @@ s.bind(('',4444))
 
 s.listen(10)
 print "socket listening"
-
+os.kill(pid, signal.SIGUSR1)
 while 1:
     connection,address = s.accept()
     data = connection.recv(1024)
