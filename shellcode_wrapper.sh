10f184d9fd63483dc3c44f0c6a4231140168c1aa
troykkt <troydkent@gmail.com>
2016-06-27 12:01:53 -0400

    added

---
+++ shellcode_wrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SHELLCODE="$(objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+)"
+echo $SHELLCODE
+sed "s/{Shellcode_goes_here}/$SHELLCODE/" shellcode.c.template > shellcode.c
+
+gcc -fno-stack-protector -z execstack shellcode.c -o shellcode
